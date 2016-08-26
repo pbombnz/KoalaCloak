58afad20e76a52cddd2b42cfd83a2e80ee0bbb5a
Prashant B <prashant_bhikhu@hotmail.com>
2016-08-26 20:07:49 +1200

    Made a ContactActivity for contact information.

---
+++ app/src/main/java/nz/pbomb/xposed/anzmods2/activities/ContactActivity.java
@@ -0,0 +1,17 @@
+package nz.pbomb.xposed.anzmods2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import nz.pbomb.xposed.anzmods2.R;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+
+
+}
