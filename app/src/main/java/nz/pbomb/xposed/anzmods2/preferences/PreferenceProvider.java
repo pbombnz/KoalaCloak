035758b16a8d7fe1116514be65ac1b39cf07805d
Prashant B <prashant_bhikhu@hotmail.com>
2016-08-29 14:33:12 +1200

    Added Preference Provider for external SharePreferences access.

---
+++ app/src/main/java/nz/pbomb/xposed/anzmods2/preferences/PreferenceProvider.java
@@ -0,0 +1,11 @@
+package nz.pbomb.xposed.anzmods2.preferences;
+
+import com.crossbowffs.remotepreferences.RemotePreferenceProvider;
+
+import nz.pbomb.xposed.anzmods2.Common;
+
+public class PreferenceProvider extends RemotePreferenceProvider {
+    public PreferenceProvider() {
+        super("nz.pbomb.xposed.anzmods2.preferences.provider", new String[] { Common.getInstance().getAppPackage() });
+    }
+}
