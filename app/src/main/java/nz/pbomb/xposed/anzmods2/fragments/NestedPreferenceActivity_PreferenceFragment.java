991f6a59853bd87a90ba52002a8521d6afd2d3e2
Prashant B <prashant_bhikhu@hotmail.com>
2016-08-29 19:08:32 +1200

    Added code so the PreferenceFragment can read the SharedPreferences files automatically.

--- app/src/main/java/nz/pbomb/xposed/anzmods2/fragments/NestedPreferenceActivity_PreferenceFragment.java
+++ app/src/main/java/nz/pbomb/xposed/anzmods2/fragments/NestedPreferenceActivity_PreferenceFragment.java
@@ -1,6 +1,8 @@
 package nz.pbomb.xposed.anzmods2.fragments;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.v4.app.Fragment;
@@ -10,11 +12,20 @@ import android.view.ViewGroup;
 
 import nz.pbomb.xposed.anzmods2.R;
 
-public class NestedPreferenceActivity_PreferenceFragment extends PreferenceFragment {
+public class NestedPreferenceActivity_PreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(getArguments().getInt("id"));
+
+        getPreferenceManager().setSharedPreferencesMode(Context.MODE_WORLD_READABLE);
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
     }
 }
